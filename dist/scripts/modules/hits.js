58429cd28690c025f4ec22460815eb079755232d
acherkashin <a.cherkashin@notamedia.com.ru>
2025-09-17 23:44:18 +0300

    refactor: i pages

---
+++ dist/scripts/modules/hits.js
@@ -0,0 +1,13 @@
+(() => {
+  new Splide(document.querySelector("[data-component='hits-slider']"), {
+    type: 'loop',
+    arrows: false,
+    pagination: false,
+    mediaQuery: 'min',
+    gap: 2,
+    perPage: 2,
+    breakpoints: {
+      [MediaSizesValue.LG]: { perPage: 4 },
+    },
+  }).mount();
+})();
